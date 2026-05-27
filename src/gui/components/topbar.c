aea49656c201af93597191950b209c461edae9bd
rizukirr <rizkirr.xyz@gmail.com>
2026-05-27 20:25:15 +0700

    fix: add condition title on topbar

--- src/gui/components/topbar.c
+++ src/gui/components/topbar.c
@@ -21,8 +21,18 @@ void TopBar(void) {
           .layout = {.sizing = {.height = Fixed(18), .width = Fixed(18)}});
     Column("CurrentLocation", .layout = {.sizing = {.height = Grow(), .width = Grow()},
                                          .childAlignment = {.y = AlignYCenter()}}) {
-      Text(TextFormat("%s, %s", cfg->city, getCountryName(cfg->country)), .fontId = a->fontBold,
-           .textColor = COLOR_PRIMARY, .fontSize = FONT_SIZE_TITLE_LARGE);
+
+      const char *location = NULL;
+      if (cfg->city[0] != '\0' && cfg->country[0] != '\0') {
+        location = TextFormat("%s, %s", cfg->city, getCountryName(cfg->country));
+      } else if (cfg->city[0] != '\0') {
+        location = cfg->city;
+      } else if (cfg->country[0] != '\0') {
+        location = getCountryName(cfg->country);
+      }
+
+      Text(location, .fontId = a->fontBold, .textColor = COLOR_PRIMARY,
+           .fontSize = FONT_SIZE_TITLE_LARGE);
       Text(TextFormat("%.6f, %.6f", cfg->latitude, cfg->longitude),
            .textColor = COLOR_ON_BACKGROUND, .fontSize = FONT_SIZE_TITLE_MEDIUM,
            .wrapMode = CC_TEXT_WRAP_WORDS);
